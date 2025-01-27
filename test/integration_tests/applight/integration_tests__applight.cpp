8c9572ebf45a871af1db7ca08257828a0f0c37d4
royyandzakiy <royyan.abdul@gmail.com>
2025-01-28 05:53:34 +0700

    added to readme

--- test/integration_tests/applight/integration_tests__applight.cpp
+++ test/integration_tests/applight/integration_tests__applight.cpp
@@ -5,5 +5,7 @@
 extern "C" void app_main()
 {
     ::testing::InitGoogleTest();
+    // ::testing::GTEST_FLAG(filter) = "LightTest";
+    ::testing::GTEST_FLAG(filter) = "LightTest.should_set_gpio_2_to_high_when_applight_on";
     RUN_ALL_TESTS();
 }
