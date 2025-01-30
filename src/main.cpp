e44c84424ec81e4b315f3459914051a85c7ece94
royyandzakiy <royyan.abdul@gmail.com>
2025-01-30 21:44:56 +0700

    added main plans

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * - wifi & mqtt init, connect
+ *      - event logger & led working (just running)
+ * - ota check (& perform if needed)
+ * - sdcard check
+ * - sensormanager sensors activate
+ */
+
 int main() {
     
 }
