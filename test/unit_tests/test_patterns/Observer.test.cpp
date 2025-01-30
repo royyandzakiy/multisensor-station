05ba1e426b472698f47e01f5b5362fc5b0cc2137
royyandzakiy <royyan.abdul@gmail.com>
2025-01-31 04:09:52 +0700

    observer test: added all test cases

--- test/unit_tests/test_patterns/Observer.test.cpp
+++ test/unit_tests/test_patterns/Observer.test.cpp
@@ -73,3 +73,82 @@ TEST_F(ObserverTest, should_not_notify_observer_when_it_has_been_removed) {
     // obervable calls notifyObservers
     // check through mock method if mockObserver1.notify has NOT been called
 }
+
+/**
+ * #include <gtest/gtest.h>
+#include <memory>
+#include "observable.h" // Include the header containing Observable and Observer
+
+// Define a mock observer
+class MockObserver : public Observer {
+public:
+    MOCK_METHOD(void, notify, (std::weak_ptr<Observable> observable), (override));
+};
+
+// Test fixture for Observable
+class ObservableTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        observable = std::make_shared<Observable>();
+        observer1 = std::make_shared<MockObserver>();
+        observer2 = std::make_shared<MockObserver>();
+    }
+
+    std::shared_ptr<Observable> observable;
+    std::shared_ptr<MockObserver> observer1;
+    std::shared_ptr<MockObserver> observer2;
+};
+
+TEST_F(ObservableTest, should_notify_observer_when_notifyObservers_is_called) {
+    observable->addObserver(observer1);
+    EXPECT_CALL(*observer1, notify(::testing::_)).Times(1);
+    observable->notifyObservers();
+}
+
+TEST_F(ObservableTest, should_notify_all_observers_when_notifyObservers_is_called) {
+    observable->addObserver(observer1);
+    observable->addObserver(observer2);
+    EXPECT_CALL(*observer1, notify(::testing::_)).Times(1);
+    EXPECT_CALL(*observer2, notify(::testing::_)).Times(1);
+    observable->notifyObservers();
+}
+
+TEST_F(ObservableTest, should_not_notify_any_observer_when_no_observers_are_added) {
+    EXPECT_CALL(*observer1, notify(::testing::_)).Times(0);
+    observable->notifyObservers();
+}
+
+TEST_F(ObservableTest, should_not_notify_observer_when_it_has_been_removed) {
+    observable->addObserver(observer1);
+    observer1.reset();
+    EXPECT_CALL(*observer1, notify(::testing::_)).Times(0);
+    observable->notifyObservers();
+}
+
+TEST_F(ObservableTest, should_receive_valid_weak_ptr_when_notified) {
+    observable->addObserver(observer1);
+    EXPECT_CALL(*observer1, notify(::testing::_)).WillOnce([](std::weak_ptr<Observable> obs) {
+        EXPECT_FALSE(obs.expired());
+    });
+    observable->notifyObservers();
+}
+
+TEST_F(ObservableTest, should_receive_expired_weak_ptr_when_observable_is_destroyed) {
+    std::weak_ptr<Observable> weakObservable;
+    {
+        auto tempObservable = std::make_shared<Observable>();
+        weakObservable = tempObservable;
+        tempObservable->addObserver(observer1);
+    }
+    EXPECT_CALL(*observer1, notify(::testing::_)).WillOnce([](std::weak_ptr<Observable> obs) {
+        EXPECT_TRUE(obs.expired());
+    });
+    observer1->notify(weakObservable);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+ */
